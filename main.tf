edf2963d17c58681a5bd6d953636940558d98d17
wlados <wlad1324@gmail.com>
2022-03-21 13:55:51 +0200

    update: increase init time for VMs

--- main.tf
+++ main.tf
@@ -241,7 +241,7 @@ resource "google_compute_instance_group_manager" "server_group" {
 
   auto_healing_policies {
     health_check      = google_compute_health_check.health_8080.id
-    initial_delay_sec = 300
+    initial_delay_sec = 360
   }
 }
 
@@ -255,7 +255,7 @@ resource "google_compute_autoscaler" "autoscaler_servers" {
   autoscaling_policy {
     max_replicas    = 3
     min_replicas    = 1
-    cooldown_period = 300
+    cooldown_period = 360
 
     cpu_utilization {
       target = 0.75
